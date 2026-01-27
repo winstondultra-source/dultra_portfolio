068ac170ade15977f13957981e1f1dabc40e1dff
Winston Dultra <cyril.oraliza@urios.edu.ph>
2026-01-27 16:35:41 +0800

    Add Footer component to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
 import Learning from '@/components/Learning';
 import ContactForm from '@/components/ContactForm';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <Learning />
       </div>
       <ContactForm />
+      <Footer />
     </div>
   );
 }
